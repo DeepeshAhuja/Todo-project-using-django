3558d883d097f42ccfb6148c36228d923ec00325
DeepeshAhuja <deepeshahuja2@gmail.com>
2023-12-08 00:04:53 +0530

    added delete functionality

--- new_app/urls.py
+++ new_app/urls.py
@@ -3,4 +3,5 @@ from new_app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('done/<int:pk>/', views.delete, name='delete'),
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -15,3 +15,12 @@ def home(request):
     context = {'tasks': tasks}
     
     return render(request, 'index.html', context)
+
+def delete(request, pk):
+
+    task = TodoList.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+    context = {'task_name': task.task_name}
+    return render(request, 'delete.html', context)
