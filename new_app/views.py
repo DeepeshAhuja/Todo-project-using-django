d19718b85649dc962f192de265ea62b4068b4f45
DeepeshAhuja <deepeshahuja2@gmail.com>
2023-12-07 23:49:04 +0530

    added add function

--- new_app/views.py
+++ new_app/views.py
@@ -5,6 +5,13 @@ from new_app.models import TodoList
 def home(request):
     
     tasks = TodoList.objects.all()
+
+    if request.method == 'POST':
+        new_task = request.POST.get('new_task')
+        priority = request.POST.get('priority')
+        task = TodoList(task_name=new_task, priority=priority)
+        task.save()
+    
     context = {'tasks': tasks}
     
     return render(request, 'index.html', context)
